db54eef6ebbf726d5fd40d0fbf29356f4a171a07
Yuanyinz <zyyzhongyuanying@gmail.com>
2023-05-24 17:56:18 +0800

    function and icon

--- Dom/newScript.js
+++ Dom/newScript.js
@@ -1,6 +1,36 @@
 const button1 = document.querySelector('button');
 console.log(button1);
 
+
+const handleDelete = (e) => {
+  e.target.parentNode.remove();
+}
+
+
+// let confirmbutton;
+
+const handleEdit = (e) => {
+  const newinput = document.createElement('input');
+  e.target.parentNode.appendChild(newinput);
+  const confirmbutton = document.createElement('button');
+  confirmbutton.textContent = 'confirm';
+  e.target.parentNode.appendChild(confirmbutton);
+  confirmbutton.addEventListener('click', handleConfirm)
+}
+
+const handleConfirm = (e) => {
+  const newInput = e.target.previousSibling.value;
+  e.target.parentNode.querySelector('p').innerText = newInput;
+  e.target.previousSibling.remove();
+  e.target.remove();
+}
+
+//add web favicon
+//add style sheet
+//research bootstrap
+
+
+
 button1.addEventListener('click', () => {
     console.log('button clicked');
     //select target input
@@ -8,29 +38,61 @@ button1.addEventListener('click', () => {
     console.log('input value', input.value)
     //create element with the value
     const plan = document.createElement('p');
+    plan.style.display = 'inline-block';
     plan.innerText = input.value
+
+
+    //create a container div
+    const container = document.createElement('div');
+    container.appendChild(plan);
 
     // 创造两个新button
     const deletebutton = document.createElement('button');
     deletebutton.textContent = 'Delete';
-    plan.appendChild(deletebutton);
+    container.appendChild(deletebutton);
 
     const editbutton = document.createElement('button');
     editbutton.textContent = 'Edit';
-    plan.appendChild(editbutton);
+    container.appendChild(editbutton);
 
     //div to contain plan and the remote button
-    deletebutton.addEventListener('click', () => {
-      console.log('button clicked');
-      plan.remove();
-      })
+    //删除键
+    deletebutton.addEventListener('click', handleDelete)
 
-    editbutton.addEventListener('click', () => {
-      console.log('button clicked');
-      const newinput = document.querySelector('input');
-      console.log(newinput);
-      })
+    //  编辑键
+    editbutton.addEventListener('click', handleEdit)
+
+    // editbutton.addEventListener('click', (e) => {
+    //   console.log('edit button clicked', e.target)
+
+    //   const newinput = document.createElement('input');
+    //   container.appendChild(newinput);
+      //新的button
+      // const confirmbutton = document.createElement('button');
+      // confirmbutton.textContent = 'confirm';
+      // container.appendChild(confirmbutton);
+
+
+      //点击button 以后
+    // confirmbutton.addEventListener('click', handleConfirm)
+
+      // confirmbutton.addEventListener('click', (e) => {
+      //   console.log('confirm button clicked');
+      //   //e.target => confirm button
+      //   // e.target.parentNode => container
+      //   // console.log('access the target => p', e.target.parentNode.querySelector('p'))
+
+      //   const newInput = e.target.previousSibling.value;
+      //   e.target.parentNode.querySelector('p').innerText = newInput;
+
+      //   //删除 文字框和button
+      //   confirmbutton.remove();
+      //   newinput.remove();
+        
+      //   })
+      // })
     //add the plan node to the DOM tree
-    document.querySelector('.greeting').appendChild(plan)
+    document.querySelector('.toDoList').appendChild(container);
   
-  })
+  }
+)
