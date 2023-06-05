ac49184c92a0f216a70f5b00132e305aee609e84
Yuanyinz <zyyzhongyuanying@gmail.com>
2023-06-06 01:36:37 +0800

    delete edit

--- week5/server/server.js
+++ week5/server/server.js
@@ -3,18 +3,80 @@ const app = express();
 const path = require('path');
 const PORT = 8080;
 
-// serve index.html
-app.get('/', (req, res) => {
-  return res
-    .status(200)
-    //为什么改了postman没反应？还是 200 ok?
-    .sendFile(path.resolve(__dirname, '../client/index.html'));
-});
+app.use(express.json());
 
-app.post('/test',(req,res,next) => {
-  res.send('test post')
+const data = {
+  "studying": "studying",
+  "shop": "shop",
+}
+
+// serve index.html
+// app.get('/', (req, res) => {
+//   return res
+//     .status(200)
+//     //为什么改了postman没反应？还是 200 ok?
+//     .sendFile(path.resolve(__dirname, '../client/index.html'));
+// });
+
+
+// user get information from data
+app.get('/', (req, res, next) => {
+  // console.log('the response will be sent by the next function ...')
+  console.log('do I have access to the data?', data)
+  res.send(data)
   next()
 })
+
+
+//user add new toDo 
+app.post('/',(req,res,next) => {
+  console.log('do I have info from req.body?', req.body.toDo)
+  Object.assign(data, req.body.toDo);
+  //also can use Object.keys()
+  console.log('data', data);
+
+  res.send("success");
+  next();
+})
+
+
+//user delete toDo
+app.delete('/',(req,res,next) =>{
+  console.log('info from req.body',req.body)
+  const key = req.body.key
+  delete data[key]
+  console.log(data)
+  res.send('success')
+  next()
+})
+
+//user edit toDo
+app.put('/',(req,res,next) => {
+  console.log('update info from req.body',req.body)
+  for (let key in data) {
+    if (Object.keys(req.body).includes(key)) {
+      data[key] = req.body[key];
+    }
+  }
+  console.log(data)
+  res.send('success')
+  next()
+})
+// original key and value;
+// new key and value
+
+
+
+//Handle non-existed url request
+app.use((req, res, next) => {
+  res.status(404).send('Page not found');
+});
+
+
+//Global error handler
+
+
+
 
 // start server
 app.listen(PORT, () => {
@@ -22,6 +84,13 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+
+
+
+
+
+
 
 
 // get
@@ -43,6 +112,12 @@ module.exports = app;
 //   }, (req, res) => {
 //     res.send('Hello from B!')
 //   })
+
+// app.post('/test',(req,res,next) => {
+//   res.send('test post')
+//   console.log("?")
+//   next()
+// })
 
 
 
